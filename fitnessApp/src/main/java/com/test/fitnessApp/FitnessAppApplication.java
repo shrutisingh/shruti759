055d0f0d58c1ac09d21f4ae95c00643f8b2bbb81
Singh <satyam.singh@honeywell.com>
2017-10-28 11:12:00 +0530

    first commit

---
+++ fitnessApp/src/main/java/com/test/fitnessApp/FitnessAppApplication.java
@@ -0,0 +1,12 @@
+package com.test.fitnessApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAppApplication.class, args);
+	}
+}
---
+++ fitnessApp/src/main/java/com/test/fitnessApp/dao/Person.java
@@ -0,0 +1,5 @@
+package com.test.fitnessApp.dao;
+
+public class Person {
+	String personName;
+}
